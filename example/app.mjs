f206ad3d3ed96063e468873b26259321761e07f1
jascha <jascha@jaeh.at>
2020-02-11 07:29:59 +0100

    export theme vars from app.mjs for use in example page

--- example/app.mjs
+++ example/app.mjs
@@ -11,4 +11,47 @@ export const state = {
       ],
     },
   ],
+  vars: {
+  neutral: '#5a5a5a',
+  text: {
+    dark: '#c4c4c4',
+    light: '#212121',
+  },
+  link: {
+    dark: '#c4c4c4',
+    light: '#212121',
+    hover: {
+      dark: '#ea4444',
+      light: '#ea4444',
+    },
+  },
+  background: {
+    backgroundDark: '#212121',
+    backgroundLight: '#e0e0e0',
+  },
+
+  primary: {
+    neutral: '#ea4444',
+    light: '#ff7070',
+    dark: '#c82222',
+  },
+
+  secondary: {
+    neutral: '#55aa55',
+    light: '70ff70',
+    dark: '22c822',
+  },
+
+  white: '#ffffff',
+
+  maxWidth: '1000px',
+
+  fadeDuration: '500ms',
+
+  widths: {
+    tablet: 500,
+    laptop: 1024,
+    desktop: 1600,
+  },
+}
 }
