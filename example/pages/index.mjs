7f03613cd8d0184fa2793b2e8ac6b193c2c4a460
jascha <jascha@jaeh.at>
2020-02-11 04:17:40 +0100

    add theme vars to doc page

--- example/pages/index.mjs
+++ example/pages/index.mjs
@@ -22,5 +22,68 @@ export default {
   THEME: '@webboot/magic-theme',
 }`),
 
+    h3({ id: 'colors' }, 'colors'),
+    p('@webboot/magic-theme exports and uses the following default colors:'),
+    ul(
+      [
+        ['grey                ', '#5a5a5a'],
+        ['darkText            ', '#c4c4c4'],
+        ['backgroundDark      ', '#212121'],
+        ['backgroundLight     ', '#e0e0e0'],
+        ['lightText           ', '#212121'],
+        ['red                 ', '#ea4444'],
+        ['redLight            ', '#ff7070'],
+        ['redDark             ', '#c42222'],
+        ['white               ', '#ffffff'],
+        ['linkColor           ', '#c4c4c4'],
+        ['linkColorLight      ', '#212121'],
+        ['linkColorHover      ', '#ea4444'],
+        ['linkColorHoverLight ', '#ea4444'],
+      ].map(([name, color]) =>
+        li([span(`${name}: `), span({ class: 'Bg', style: `background-color: ${color}` }, color)]),
+      ),
+    ),
+
+    h4('max page width'),
+    p('vars.maxWidth: ', '1000px'),
+
+    h4('fade duration:'),
+    p('vars.fadeDuration should be used for all css animations'),
+    p(['fadeDuration: ', '500ms']),
+
+    h4('widths'),
+    p('the following width breakpoints exist:'),
+    ul(
+      [
+        ['tablet', '500px'],
+        ['laptop', '1024px'],
+        ['desktop', '1600px'],
+      ].map(([name, width]) => li([name, ': ', width])),
+    ),
+
     p('magic will then find and merge the themes automatically, just as it did on this page'),
   ])
+
+export const style = {
+  ul: {
+    display: 'inline-block',
+    width: '240px',
+  },
+  li: {
+    display: 'inline-block',
+    width: '100%',
+  },
+  span: {
+    float: 'left',
+  },
+
+  '.Bg': {
+    border: '1px solid #ffffff',
+    borderRadius: '0.3em',
+    color: 'rebeccapurple',
+    float: 'right',
+    padding: '0.1em',
+    textAlign: 'right',
+    width: '75px',
+  },
+}
