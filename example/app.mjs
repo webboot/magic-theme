2a6869aae5cd347cac04a1f3f520a5aeb8de48ab
jascha <jascha@jaeh.at>
2020-02-11 08:31:32 +0100

    update app themevars

--- example/app.mjs
+++ example/app.mjs
@@ -12,46 +12,50 @@ export const state = {
     },
   ],
   vars: {
-  neutral: '#5a5a5a',
-  text: {
-    dark: '#c4c4c4',
-    light: '#212121',
-  },
-  link: {
-    dark: '#c4c4c4',
-    light: '#212121',
-    hover: {
-      dark: '#ea4444',
-      light: '#ea4444',
+    background: {
+      dark: '#212121',
+      light: '#e0e0e0',
+    },
+
+    link: {
+      dark: '#c4c4c4',
+      light: '#212121',
+
+      hover: {
+        dark: '#ea4444',
+        light: '#ea4444',
+      },
+    },
+
+    neutral: '#5a5a5a',
+
+    primary: {
+      dark: '#c82222',
+      light: '#ff7070',
+      neutral: '#ea4444',
+    },
+
+    secondary: {
+      dark: '#22c822',
+      light: '#70ff70',
+      neutral: '#55aa55',
+    },
+
+    text: {
+      dark: '#c4c4c4',
+      light: '#212121',
+    },
+
+    white: '#ffffff',
+
+    maxWidth: '1000px',
+
+    fadeDuration: '500ms',
+
+    widths: {
+      tablet: 500,
+      laptop: 1024,
+      desktop: 1600,
     },
   },
-  background: {
-    backgroundDark: '#212121',
-    backgroundLight: '#e0e0e0',
-  },
-
-  primary: {
-    neutral: '#ea4444',
-    light: '#ff7070',
-    dark: '#c82222',
-  },
-
-  secondary: {
-    neutral: '#55aa55',
-    light: '70ff70',
-    dark: '22c822',
-  },
-
-  white: '#ffffff',
-
-  maxWidth: '1000px',
-
-  fadeDuration: '500ms',
-
-  widths: {
-    tablet: 500,
-    laptop: 1024,
-    desktop: 1600,
-  },
-}
 }
